4cb574055a850ce9b97adef946546f7275ea0dfc
BuildTools <unconfigured@null.spigotmc.org>
2023-04-01 19:46:44 +0800

    First Commit

---
+++ src/main/java/sprucegoose/avatarmc/AvatarMC.java
@@ -0,0 +1,17 @@
+package sprucegoose.avatarmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AvatarMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
